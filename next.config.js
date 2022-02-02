d14b20ca1e797531076bf8e283fafa98092ad71d
semigura <shotaro-ishigaki@gaji.jp>
2022-02-02 22:56:02 +0900

    Add service worker

--- next.config.js
+++ next.config.js
@@ -11,3 +11,12 @@ module.exports = {
     return config;
   },
 };
+
+const withPWA = require("next-pwa");
+
+module.exports = withPWA({
+  pwa: {
+    dest: "public", // swの出力ディレクトリ
+    // runtimeCaching: []
+  },
+});
